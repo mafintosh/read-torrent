885adfaa80d0884eeaf4a9d2a1647e7d61113d5e
Mathias Buus <mathiasbuus@gmail.com>
2013-08-08 22:03:19 +0200

    0.2.0 - more offsets - better naming and formatting in cli tool

--- cli.js
+++ cli.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
 var readTorrent = require('./index');
+var units = ['B','KB','MB','GB'];
 
 var pad = function(s) {
 	return s < 10 ? '0'+s : s;
 };
-var format = function(d) {
+var formatDate = function(d) {
 	return pad(d.getUTCDate())+'/'+pad(d.getUTCMonth()+1)+'/'+d.getUTCFullYear();
+};
+var formatSize = function(s, mag) {
+	mag = mag || 0;
+	if (mag < 3 && s > (1 << 10*(mag+1))) return formatSize(s, mag+1);
+	return (Math.round(10*(s >> 10*mag-3) / 8)/10) + units[mag];
 };
 
 if (!process.argv[2]) {
@@ -16,13 +22,13 @@ if (!process.argv[2]) {
 
 readTorrent(process.argv[2], function(err, torrent) {
 	console.log('info hash: '+torrent.infoHash);
-	console.log('created:   '+format(torrent.created));
-	console.log('pieces:    '+torrent.pieces.length + ' x '+torrent.pieceLength);
+	console.log('created:   '+formatDate(torrent.created));
+	console.log('pieces:    '+torrent.pieces.length + ' x '+formatSize(torrent.pieceLength));
 
 	console.log('name:      '+torrent.name);
-	console.log('files:     '+torrent.files[0].name+ ' ('+torrent.files[0].length+')');
+	console.log('files:     '+torrent.files[0].name+ ' ('+formatSize(torrent.files[0].length)+')');
 	for (var i = 1; i < torrent.files.length; i++) {
-		console.log('           '+torrent.files[i].name+ ' ('+torrent.files[i].length+')');
+		console.log('           '+torrent.files[i].name+ ' ('+formatSize(torrent.files[i].length)+')');
 	}
 
 	console.log('trackers:  '+torrent.announce[0]);
--- index.js
+++ index.js
@@ -22,6 +22,8 @@ var parse = function(torrent) {
 
 	var result = {};
 	var name = torrent.info.name.toString();
+	var pieceLength = torrent.info['piece length'];
+	var files = (torrent.info.files || [torrent.info]);
 
 	result.created = new Date(torrent['creation date'] * 1000);
 	result.announce = (torrent['announce-list'] || [torrent.announce]).map(function(obj) {
@@ -30,23 +32,30 @@ var parse = function(torrent) {
 	result.infoHash = crypto.createHash('sha1').update(bncode.encode(torrent.info)).digest('hex');
 	result.private = !!torrent.info.private;
 	result.name = name;
+	result.length = files.reduce(sumLength, 0);
 
-	result.files = (torrent.info.files || [torrent.info]).map(function(file, i, files) {
+	result.files = files.map(function(file, i) {
 		var parts = [].concat(file.name || name, file.path || []).map(function(p) {
 			return p.toString();
 		});
-		return {
-			path: path.join.apply(null, [path.sep].concat(parts)).slice(1),
-			name: parts.pop(),
-			length: file.length,
-			offset: files.slice(0, i).reduce(sumLength, 0)
-		};
+
+		var result = {};
+
+		result.path = path.join.apply(null, [path.sep].concat(parts)).slice(1);
+		result.name = parts.pop();
+		result.length = file.length;
+		result.offset = files.slice(0, i).reduce(sumLength, 0);
+		result.end = result.offset + result.length - 1;
+		result.offsetPiece = (result.offset / pieceLength) | 0;
+		result.endPiece = (result.end / pieceLength) | 0;
+
+		return result;
 	});
 
 	var lastFile = result.files[result.files.length-1];
 
-	result.pieceLength = torrent.info['piece length'];
-	result.lastPieceLength = ((lastFile.offset + lastFile.length) % result.pieceLength) || result.pieceLength;
+	result.pieceLength = pieceLength;
+	result.pieceRemainder = (result.length % pieceLength) || pieceLength;
 	result.pieces = splitPieces(torrent.info.pieces);
 
 	return result;
